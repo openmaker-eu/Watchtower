f43abab70f0dd54ed5d8245f4caec76025f9b90f
Enis <enisimsar@gmail.com>
2017-01-27 15:25:10 +0300

    sad

--- manageAll.py
+++ manageAll.py
@@ -1,8 +1,10 @@
 from addTrack import AddTrack
 from app import webserverInit
+from logic import getAlertList
 import sys
 reload(sys)
 sys.setdefaultencoding('utf8')
 
 mainT = AddTrack()
+mainT.setup(getAlertList())
 webserverInit(mainT)
