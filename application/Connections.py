78d9311438db2c0260ccd36d9b791114796e8b62
Enis Simsar <enis.simsar@boun.edu.tr>
2017-03-10 15:12:42 +0200

    Update Connections.py

--- application/Connections.py
+++ application/Connections.py
@@ -13,6 +13,8 @@ class Connection:
         try:
             self.MongoDBClient = pymongo.MongoClient('mongodb://admin:smio1EUp@138.68.92.181:27017/')
             self.db = self.MongoDBClient.openMakerdB
+            self.feedDB = self.MongoDBClient.feedDB
+            self.infDB = self.MongoDBClient.influenceRanks
             self.PostGreSQLConnect = psycopg2.connect("dbname='openmakerdb' user='openmakerpsql' host='138.68.92.181' password='smio1EUp'")
             self.cur = self.PostGreSQLConnect.cursor()
             print "new connection"
