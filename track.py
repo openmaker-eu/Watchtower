a6fb52e908ebfea7bcaeddb964fb95618f6d2566
Enis Simsar <enisimsar@gmail.com>
2017-03-15 23:58:03 +0300

    api etditting

--- track.py
+++ track.py
@@ -20,16 +20,11 @@ def get_next_tweets_sequence():
     )
     return cursor['seq']
 
-#Variables that contains the user credentials to access Twitter API
-access_token = "455237513-3NZrkuCQMOldFRhuC53r8WoKDY7T5MxjqYyWgzHb"
-access_token_secret = "YnCFJANRcEnPPqUM8TcbVbHS0lQX3KjQnKdPU3XdG2sTy"
-consumer_key = "K6Rk0fGEFB6gtKYuzaBnTMT9T"
-consumer_secret = "F3NryI1Nw3fXGawq2DYQBeowmzsmxWba4wLgPKyUucudwJ35mY"
-
+# Accessing Twitter API
 consumer_key = "utTM4qfuhmzeLUxRkBb1xb12P" # API key
 consumer_secret = "XteCQjAZCVAu7Tk5ftgcjv0jJlII2o7b8BqZc3sfEdwn1R6Ic7" # API secret
 access_token = "821415961467228161-iB85g0Lm8c4jLqIqxWcryWjE8nm6CPq"
-access_token_secret = "BrNaqN0BP2K3rYzIurlaTIaJeOk4MBP6mzBtR73ay5ulU"
+access_secret = "BrNaqN0BP2K3rYzIurlaTIaJeOk4MBP6mzBtR73ay5ulU"
 
 #This is a basic listener that just prints received tweets to stdout.
 class StdOutListener(StreamListener):
