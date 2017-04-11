1b9039d75fafb4a72c1a88505a8fac1972d00695
Enis Simsar <enisimsar@gmail.com>
2017-04-12 01:08:01 +0300

    clear

--- manageAll.py
+++
@@ -1,7 +0,0 @@
-from addAlert import AddAlert
-from app import webserverInit
-from logic import getAllRunningAlertList
-
-mainT = AddAlert()
-mainT.setup(getAllRunningAlertList())
-webserverInit(mainT)
