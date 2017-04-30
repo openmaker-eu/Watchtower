56f5e543c460f17eaac4f4aafaf03a7f570a4387
Enis Simsar <enisimsar@gmail.com>
2017-04-30 19:45:09 +0300

    newapi.py

--- newapi.py
+++ newapi.py
@@ -17,6 +17,7 @@ def getFeeds(themename, userid, date, cursor):
         result['Error'] = 'invalid date'
         return json.dumps(result, indent=4)
     themeid = str(logic.getAlertIdwithUserId(themename, userid))
+    print themeid
     feeds = list(Connection.Instance().newsdB[themeid].find({'name': date}, {date: 1}))
     print feeds
     feeds = list(feeds[0][date][cursor:cursor+20])
