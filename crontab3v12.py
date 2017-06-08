2a947167db57d9d8ab797d1e342cd6d2f8643540
Enis Simsar <enisimsar@gmail.com>
2017-06-08 22:35:19 +0300

    NewsPool

--- crontab3v12.py
+++ crontab3v12.py
@@ -75,7 +75,7 @@ def main():
     alertid_list = sorted(list(Connection.Instance().cur.fetchall()))
     print(alertid_list)
     alertid_list = [alertid[0] for alertid in alertid_list]
-    pool = ThreadPool(3, False)
+    pool = ThreadPool(2, False)
     pool.map(calculateLinks, alertid_list)
     pool.wait_completion()
 
