f16b90639d8f020fd707a0759fc01ca0194c5610
Enis Simsar <enisimsar@gmail.com>
2017-06-09 14:44:12 +0300

    Requirement.txt

--- crontab3v12.py
+++ crontab3v12.py
@@ -75,6 +75,7 @@ def main():
     alertid_list = sorted(list(Connection.Instance().cur.fetchall()))
     print(alertid_list)
     alertid_list = [alertid[0] for alertid in alertid_list]
+    alertid_list = [34, 35]
     pool = ThreadPool(2, False)
     pool.map(calculateLinks, alertid_list)
     pool.wait_completion()
