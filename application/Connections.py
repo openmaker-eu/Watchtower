a9f24a0315c134b6f70ff5cc084a08fb549f025a
Enis Simsar <enisimsar@gmail.com>
2017-09-14 18:10:25 +0300

    Connection changed.

--- application/Connections.py
+++ application/Connections.py
@@ -4,15 +4,34 @@ import psycopg2
 import pymongo
 from contextlib import contextmanager
 from psycopg2.pool import ThreadedConnectionPool
+import urllib.request
+import re
 
 from application.utils.Singleton import Singleton
-
 
 @Singleton
 class Connection:
     def __init__(self):
         try:
-            self.MongoDBClient = pymongo.MongoClient('mongodb://admin:smio1EUp@138.68.92.181:27017/', connect=False)
+            hosts = ['138.68.92.181']
+
+            with urllib.request.urlopen('http://ipinfo.io/ip') as response:
+                html = response.read()
+                
+            a = re.findall("\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}", str(html))
+
+            h = a[0]
+
+            if h in hosts:
+                host = h
+            else:
+                print("Please select a host: ")
+                for i in range(0, len(hosts)):
+                    print(str(i+1) + "->" + hosts[i])
+
+                host = hosts[int(input())-1]
+
+            self.MongoDBClient = pymongo.MongoClient('mongodb://admin:smio1EUp@'+host+':27017/', connect=False)
             self.db = self.MongoDBClient.openMakerdB
             self.newsdB = self.MongoDBClient.newsdB
             self.feedDB = self.MongoDBClient.feedDB
@@ -24,12 +43,12 @@ class Connection:
             self.filteredNewsPoolDB = self.MongoDBClient.filteredNewsPool
             self.infDB = self.MongoDBClient.influenceRanks
             self.PostGreSQLConnect = psycopg2.connect(
-                "dbname='openmakerdb' user='openmakerpsql' host='138.68.92.181' password='smio1EUp'")
+                "dbname='openmakerdb' user='openmakerpsql' host="+host+" password='smio1EUp'")
             self.cur = self.PostGreSQLConnect.cursor()
 
             self.pg_pool = psycopg2.pool.ThreadedConnectionPool(
                 1, 15,
-                host='138.68.92.181',
+                host=host,
                 port='5432',
                 user='openmakerpsql',
                 password='smio1EUp',
