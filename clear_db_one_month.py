336e732d505edbf64151143c1b39d19c25732ee8
Enis Simsar <enisimsar@gmail.com>
2017-11-28 16:06:58 +0300

    clear db update.

--- clear_db_one_month.py
+++ clear_db_one_month.py
@@ -4,11 +4,18 @@ import dateutil.relativedelta
 from application.Connections import Connection
 
 prev_month_datetime = datetime.datetime.now() + dateutil.relativedelta.relativedelta(months=-1)
+prev_two_week_datetime = datetime.datetime.now() + dateutil.relativedelta.relativedelta(weeks=-2)
 prev_month_objectid = ObjectId.from_datetime(prev_month_datetime)
+prev_two_week_objectid = ObjectId.from_datetime(prev_two_week_datetime)
 
 print(prev_month_objectid)
 
+for topic_id in Connection.Instance().db.collection_names():
+    if topic_id != "counters":
+        Connection.Instance().db[str(topic_id)].remove({'_id': {'$lte': prev_two_week_objectid}})
+        Connection.Instance().db.command("compact", str(topic_id))
+
 for topic_id in Connection.Instance().newsPoolDB.collection_names():
     if topic_id != "counters":
-        Connection.Instance().db[str(topic_id)].remove({'_id': {'$lte': prev_month_objectid}})
-        Connection.Instance().db.command("compact", str(topic_id))
+        Connection.Instance().newsPoolDB[str(topic_id)].remove({'_id': {'$lte': prev_month_objectid}})
+        Connection.Instance().newsPoolDB.command("compact", str(topic_id))
