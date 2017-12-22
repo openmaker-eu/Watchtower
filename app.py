402e4034cdb3550d0a2c059de3b5c231c0c97c8d
Kemal Berk Kocabagli <berk.kocabagli@boun.edu.tr>
2017-12-23 00:34:33 +0300

    Recommendations page finished. Needs minor improvements.

--- app.py
+++ app.py
@@ -93,6 +93,10 @@ class Application(tornado.web.Application):
             (r"/get_news/(.*)", SearchNewsHandler, {'mainT': mainT}),
             (r"/Audience/(.*)", AudienceHandler, {'mainT': mainT}),
             (r"/Audience", AudienceHandler, {'mainT': mainT}),
+
+            (r"/Recommendations/(.*)", RecommendationsHandler, {'mainT': mainT}), # added for recommendations
+            (r"/Recommendations", RecommendationsHandler, {'mainT': mainT}),
+
             (r"/previewNews", PreviewNewsHandler, {'mainT': mainT}),
             (r"/previewConversations", PreviewConversationHandler, {'mainT': mainT}),
             (r"/previewEvents", PreviewEventHandler, {'mainT': mainT}),
@@ -864,6 +868,76 @@ class SearchNewsHandler(BaseHandler, TemplateRendering):
         content = self.render_template(template, variables)
         self.write(content)
 
+class RecommendationsHandler(BaseHandler, TemplateRendering):
+    @tornado.web.authenticated
+    def get(self, argument=None):
+        user_id = tornado.escape.xhtml_escape(self.current_user)
+        template = 'afterlogintemplate.html'
+        topic = logic.getCurrentTopic(tornado.escape.xhtml_escape(self.current_user))
+        location = logic.getCurrentLocation(tornado.escape.xhtml_escape(self.current_user))
+
+        if topic is None:
+            self.redirect("/topicinfo")
+
+        audience = logic.getRecommendedAudience(topic['topic_id'], location, 'recommended', user_id, 0)
+        variables = {
+            'title': "Recommendations",
+            'alertname': topic['topic_name'],
+            'alertid': topic['topic_id'],
+            'location': location,
+            'audience': audience['audience'],
+            'cursor': audience['next_cursor'],
+            'alerts': logic.getAlertList(user_id),
+            'recom_filter':'recommended',
+            'type': "recommendations",
+            'username': str(tornado.escape.xhtml_escape(self.get_current_username())),
+            'topic': topic
+        }
+
+        content = self.render_template(template, variables)
+        self.write(content)
+
+    @tornado.web.authenticated
+    def post(self, argument=None):
+        variables = {}
+        topic = logic.getCurrentTopic(tornado.escape.xhtml_escape(self.current_user))
+        location = logic.getCurrentLocation(tornado.escape.xhtml_escape(self.current_user))
+        user_id = tornado.escape.xhtml_escape(self.current_user)
+        filter = self.get_argument('filter')
+
+        if argument is not None:
+            template = 'recommendationsTemplate.html'
+            try:
+                next_cursor = self.get_argument('next_cursor')
+            except:
+                next_cursor = 0
+                pass
+            try:
+                # filter is "rated" or "recommended"
+                audience = logic.getRecommendedAudience(topic['topic_id'], location, filter, user_id, int(next_cursor))
+                variables = {
+                    'audience': audience['audience'],
+                    'cursor': audience['next_cursor'],
+                    'recom_filter':filter
+                }
+            except Exception as e:
+                print(e)
+                self.write("")
+        else:
+            template = 'alertRecommendations.html'
+            try:
+                audience = logic.getRecommendedAudience(topic['topic_id'], location, filter, user_id, 0)
+                variables = {
+                    'audience': audience['audience'],
+                    'alertid': topic['topic_id'],
+                    'cursor': audience['next_cursor'],
+                    'recom_filter':filter
+                }
+            except Exception as e:
+                print(e)
+                self.write("")
+        content = self.render_template(template, variables)
+        self.write(content)
 
 class AudienceHandler(BaseHandler, TemplateRendering):
     @tornado.web.authenticated
