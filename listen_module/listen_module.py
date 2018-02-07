63b1b39a6b240c2cae35cdbdabfe7ed778b7ad87
Enis Simsar <enisimsar@gmail.com>
2018-02-07 15:16:22 +0300

    adding count in listen module.

--- listen_module/listen_module.py
+++ listen_module/listen_module.py
@@ -46,17 +46,20 @@ def main():
     current_hour = datetime.now().hour
     last_sequence_id = str(Connection.Instance().db["counters"].find_one({'_id': "tweetDBId"})['seq'])
 
+    count = 0
     while True:
         print("Loop is continuing")
-        sleep(450)
+        count += 1
+        sleep(300)
         new_running_topic_list = get_all_running_topics_list()
         if new_running_topic_list != running_topic_list:
             running_topic_list = new_running_topic_list
             print("Restarting Twitter Module!")
             twitter_module.restart(new_running_topic_list)
-        if current_hour != datetime.now().hour:
-            current_hour = datetime.now().hour
+        if count == 6:
+            count = 0
             new_last_sequence_id = str(Connection.Instance().db["counters"].find_one({'_id': "tweetDBId"})['seq'])
+            print("last_id = {0}, new_last_id = {1}".format(last_sequence_id, new_last_sequence_id))
             if last_sequence_id == new_last_sequence_id:
                 last_sequence_id = new_last_sequence_id
                 running_topic_list = new_running_topic_list
