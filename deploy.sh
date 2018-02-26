09dcd2d2c93d29c49dea3e728e3d1179499c0fd1
Enis Simsar <enisimsar@gmail.com>
2018-02-27 02:57:23 +0300

    auth in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ eval $(cat .env | sed 's/^/export /')
 docker-compose down
 docker-compose up -d
 
-docker exec -i postgres /usr/bin/psql -h ${HOST_IP} -U ${POSTGRESQL_USER} -d ${POSTGRESQL_DB} -a -f /data/schemas.sql
-docker exec -i postgres /usr/bin/psql -h ${HOST_IP} -U ${POSTGRESQL_USER} -d ${POSTGRESQL_DB} -f /data/relevant_locations_data.sql
-docker exec -i postgres /usr/bin/psql -h ${HOST_IP} -U ${POSTGRESQL_USER} -d ${POSTGRESQL_DB} -f /data/country_code_data.sql
-docker exec -i postgres /usr/bin/psql -h ${HOST_IP} -U ${POSTGRESQL_USER} -d ${POSTGRESQL_DB} -f /data/location_country_codes.data.sql
+echo ${POSTGRESQL_PASSWORD} | docker exec -i postgres psql -h ${HOST_IP} -U ${POSTGRESQL_USER} --password -d ${POSTGRESQL_DB} -a -f /data/schemas.sql
+echo ${POSTGRESQL_PASSWORD} | docker exec -i postgres psql -h ${HOST_IP} -U ${POSTGRESQL_USER} -d ${POSTGRESQL_DB} -f /data/relevant_locations_data.sql
+echo ${POSTGRESQL_PASSWORD} | docker exec -i postgres psql -h ${HOST_IP} -U ${POSTGRESQL_USER} -d ${POSTGRESQL_DB} -f /data/country_code_data.sql
+echo ${POSTGRESQL_PASSWORD} | docker exec -i postgres psql -h ${HOST_IP} -U ${POSTGRESQL_USER} -d ${POSTGRESQL_DB} -f /data/location_country_codes.data.sql
