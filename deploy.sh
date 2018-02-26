7d94029fad0357d1ca33c4b86c639441792f80c2
Enis Simsar <enisimsar@gmail.com>
2018-02-27 02:32:28 +0300

    Change commands in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 docker-compose down
 docker-compose up -d
 
-docker exec -it postgres psql -U ${POSTGRESQL_USER} -d ${POSTGRESQL_DB} -a -f /data/schemas.sql
-docker exec -it postgres psql -U ${POSTGRESQL_USER} -d ${POSTGRESQL_DB} -f /data/relevant_locations_data.sql
-docker exec -it postgres psql -U ${POSTGRESQL_USER} -d ${POSTGRESQL_DB} -f /data/country_code_data.sql
-docker exec -it postgres psql -U ${POSTGRESQL_USER} -d ${POSTGRESQL_DB} -f /data/location_country_codes.data.sql
+docker exec -i postgres /usr/bin/psql -h ${HOST_IP} -U ${POSTGRESQL_USER} -d ${POSTGRESQL_DB} -a -f /data/schemas.sql
+docker exec -i postgres /usr/bin/psql -h ${HOST_IP} -U ${POSTGRESQL_USER} -d ${POSTGRESQL_DB} -f /data/relevant_locations_data.sql
+docker exec -i postgres /usr/bin/psql -h ${HOST_IP} -U ${POSTGRESQL_USER} -d ${POSTGRESQL_DB} -f /data/country_code_data.sql
+docker exec -i postgres /usr/bin/psql -h ${HOST_IP} -U ${POSTGRESQL_USER} -d ${POSTGRESQL_DB} -f /data/location_country_codes.data.sql
