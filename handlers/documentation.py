2665ad6dbfdba22f3998840d70c1a3fa18a171a8
Kemal Berk Kocabagli <berk.kocabagli@boun.edu.tr>
2018-03-01 14:02:27 +0300

    Add documentation handlers

---
+++ handlers/documentation.py
@@ -0,0 +1,49 @@
+"""
+Documentation Handlers for Watchtower
+"""
+__author__ = ['Kemal Berk Kocabagli', 'Enis Simsar']
+
+from decouple import config
+
+from handlers.base import BaseHandler, TemplateRendering
+
+
+class DocumentationHandler(BaseHandler, TemplateRendering):
+    def get(self):
+        template = 'api.html'
+        variables = {
+            'title': "Watchtower Api"
+        }
+        content = self.render_template(template, variables)
+        self.write(content)
+
+
+class Documentationv11Handler(BaseHandler, TemplateRendering):
+    def get(self):
+        template = 'apiv11.html'
+        variables = {
+            'title': "Watchtower Api"
+        }
+        content = self.render_template(template, variables)
+        self.write(content)
+
+
+class Documentationv12Handler(BaseHandler, TemplateRendering):
+    def get(self):
+        template = 'apiv12.html'
+        variables = {
+            'title': "Watchtower Api v1.2"
+        }
+        content = self.render_template(template, variables)
+        self.write(content)
+
+
+class Documentationv13Handler(BaseHandler, TemplateRendering):
+    def get(self):
+        template = 'apiv13.html'
+        variables = {
+            'title': "Watchtower Api v1.3",
+            'host': config("HOST_NAME")
+        }
+        content = self.render_template(template, variables)
+        self.write(content)
