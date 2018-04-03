a94cde878f8db9be0a7c109c74a1239212f0207e
Enis Simsar <enisimsar@gmail.com>
2018-04-03 16:35:38 +0300

    filter topics

--- crontab_module/crons/get_audience_sample.py
+++ crontab_module/crons/get_audience_sample.py
@@ -160,18 +160,21 @@ def main():
         sql = (
             "SELECT topic_id, topic_name "
             "FROM topics "
+            "WHERE is_masked_location = %s"
         )
-        cur.execute(sql)
+        cur.execute(sql, [False])
         topics = cur.fetchall()  # list of all topics
         topic_dict = dict()
         for topicID, topicName in topics:
             topic_dict[topicID] = topicName
 
+        ids = [i[0] for i in topics]
         sql = (
             "SELECT user_id, topic_id "
             "FROM user_topic "
+            "WHERE topic_id IN %s"
         )
-        cur.execute(sql)
+        cur.execute(sql, tuple(ids))
         users_and_topics = cur.fetchall()  # list of all topics
 
         if (getForSpecificUsers == "1"):
