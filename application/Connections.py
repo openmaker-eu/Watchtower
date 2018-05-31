57501c9991ae16cdd0034713600b7a26b75afddb
Enis Simsar <enisimsar@gmail.com>
2018-05-31 16:28:41 +0300

    add exception to Connections singleton

--- application/Connections.py
+++ application/Connections.py
@@ -1,15 +1,14 @@
 from __future__ import print_function
 
+from contextlib import contextmanager
+
 import psycopg2
 import pymongo
-from contextlib import contextmanager
-from psycopg2.pool import ThreadedConnectionPool
-import urllib.request
-import re
-import sys
 from decouple import config
+from psycopg2.pool import ThreadedConnectionPool
 
 from application.utils.Singleton import Singleton
+
 
 @Singleton
 class Connection:
@@ -17,7 +16,9 @@ class Connection:
         try:
             host = config("HOST_IP")
 
-            self.MongoDBClient = pymongo.MongoClient('mongodb://'+config("MONGODB_USER")+":"+config("MONGODB_PASSWORD")+'@'+host+':27017/', connect=False)
+            self.MongoDBClient = pymongo.MongoClient(
+                'mongodb://' + config("MONGODB_USER") + ":" + config("MONGODB_PASSWORD") + '@' + host + ':27017/',
+                connect=False)
             self.db = self.MongoDBClient.openMakerdB
             self.newsdB = self.MongoDBClient.newsdB
             self.feedDB = self.MongoDBClient.feedDB
@@ -33,13 +34,12 @@ class Connection:
 
             self.challengesDB = self.MongoDBClient.challenges
 
-
             # AUDIENCE
-            self.influencerDB = self.MongoDBClient.influencers_test # db for influencers
-            self.audienceDB = self.MongoDBClient.audience_test # db for audience
-            self.audience_samples_DB = self.MongoDBClient.audience_samples # db for audience samples
-            self.local_influencers_DB = self.MongoDBClient.local_influencers # db for local influencers
-            self.audience_networks_DB = self.MongoDBClient.audience_networks # db for audience networks
+            self.influencerDB = self.MongoDBClient.influencers_test  # db for influencers
+            self.audienceDB = self.MongoDBClient.audience_test  # db for audience
+            self.audience_samples_DB = self.MongoDBClient.audience_samples  # db for audience samples
+            self.local_influencers_DB = self.MongoDBClient.local_influencers  # db for local influencers
+            self.audience_networks_DB = self.MongoDBClient.audience_networks  # db for audience networks
 
             # POSTGRE - INFO ABOUT TOPICS IS HELD HERE.
             self.pg_pool = psycopg2.pool.ThreadedConnectionPool(
@@ -50,11 +50,33 @@ class Connection:
                 password=config("POSTGRESQL_PASSWORD"),
                 database=config("POSTGRESQL_DB"))
 
-
             print("new connection")
         except Exception as e:
             print(e)
             print("I am unable to connect to the database")
+
+    def __getattribute__(self, name):
+        if name == 'pg_pool':
+            return object.__getattribute__(self, name)
+        try:
+            object.__getattribute__(self, 'MongoDBClient').server_info()
+            return object.__getattribute__(self, name)
+        except:
+            count = 0
+            while count < 5:
+                object.__setattr__(self, 'MongoDBClient', pymongo.MongoClient(
+                    'mongodb://' + config("MONGODB_USER") + ":" + config("MONGODB_PASSWORD") + '@' + host + ':27017/',
+                    connect=False))
+                try:
+                    object.__getattribute__(self, 'MongoDBClient').server_info()
+                    break
+                except:
+                    pass
+                count += 1
+                if count == 5:
+                    raise Exception("I am unable to connect to the mongodb")
+
+            return object.__getattribute__(self, name)
 
     @contextmanager
     def get_cursor(self):
